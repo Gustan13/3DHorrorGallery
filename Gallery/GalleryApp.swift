56655460d51849a39b673f08b5cb1b3312b70388
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-09-26 08:27:42 -0300

    Initial Commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Gustavo Binder on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GalleryUITests/GalleryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GalleryUITestsLaunchTests.swift
+//  GalleryUITests
+//
+//  Created by Gustavo Binder on 26/09/23.
+//
+
+import XCTest
+
+final class GalleryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
