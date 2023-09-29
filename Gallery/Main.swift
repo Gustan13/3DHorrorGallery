f489292cc9967f257ed17a90d15ae6521e46ccd3
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-09-29 19:32:18 -0300

    fix: frame scaling.

---
+++ Gallery/Main.swift
@@ -0,0 +1,20 @@
+//
+//  Main.swift
+//  Gallery
+//
+//  Created by Gustavo Binder on 29/09/23.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}
